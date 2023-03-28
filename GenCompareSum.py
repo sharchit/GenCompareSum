f11b3609858c5324be5c779d0322011a9aeb6693
Archit Sharma <sh02archit@gmail.com>
2023-03-28 11:04:33 +0530

    Dependency fix #3 and removed simcse

--- GenCompareSum.py
+++ GenCompareSum.py
@@ -15,7 +15,7 @@ import time
 import shutil
 import numpy as np
 import nltk
-from simcse import SimCSE
+#from simcse import SimCSE
 import argparse
 from scipy import spatial
 from sentence_transformers import SentenceTransformer
@@ -39,7 +39,7 @@ def timer(func):
 def preprocess(document: str, stride=5, list=False) -> List[str]:
     """
     This function takes a corpus document and outputs a list of generation
-    spans where the 'stride' is the number of sentences in each section. 
+    spans where the 'stride' is the number of sentences in each section.
     """
     if list==False:
         sentences = nltk.tokenize.sent_tokenize(document)
@@ -48,7 +48,7 @@ def preprocess(document: str, stride=5, list=False) -> List[str]:
     chunks = [" ".join(sentences[i:i+stride]) for i in range(0, len(sentences), stride)]
 
     return chunks
-    
+
 
 
 def generate_salient_texts(
@@ -65,7 +65,7 @@ def generate_salient_texts(
     """
 
     input_ids = tokenizer.encode(text, return_tensors='pt').to(device)
-    
+
     salient_texts = []
     try:
         outputs = model.generate(
@@ -76,7 +76,7 @@ def generate_salient_texts(
             num_return_sequences=num_texts_per_section,
             temperature=temperature
         )
-        salient_texts = [tokenizer.decode(output, skip_special_tokens=True) for output in outputs] 
+        salient_texts = [tokenizer.decode(output, skip_special_tokens=True) for output in outputs]
     except RuntimeError:
         print(len(input_ids))
 
@@ -95,9 +95,9 @@ def get_salient_texts_across_corpus(
         block_n_gram
     ):
     """
-    This function takes a document, which is pre-split into an array, with one sentence per element.  
+    This function takes a document, which is pre-split into an array, with one sentence per element.
     The function then combines several sentences into paragraphs (num sentences in section is 'stride').
-    Several salient_texts are then geenrated per section. 
+    Several salient_texts are then geenrated per section.
     The salient_texts are combined and the most frequent k salient_texts generated from across the whole corpus are taken
     """
     text_split_into_sections = preprocess(doc_text,stride=stride,list=True)
@@ -127,12 +127,12 @@ def get_salient_texts_across_corpus(
         .groupby("gen_text") \
         .nunique() \
         .sort_values("gen_id", ascending=False)
-    
+
     top_salient_texts = list(salient_texts_grouped_tbl.index[0:top_k_salient_texts])
     top_weight = np.array(salient_texts_grouped_tbl.gen_id[0:top_k_salient_texts])
-    
-    
-    # experiment with trigram blocking version   
+
+
+    # experiment with trigram blocking version
     if (block_n_gram):
         _pred = []
         _weight = []
@@ -148,16 +148,16 @@ def get_salient_texts_across_corpus(
         _pred = _pred[np.argsort(-_weight)]
         _weight = _weight[np.argsort(-_weight)]
         top_salient_texts_trigram_block = _pred[0:top_k_salient_texts]
-        weight_trigram_block = _weight[0:top_k_salient_texts]      
+        weight_trigram_block = _weight[0:top_k_salient_texts]
 
         return top_salient_texts, top_weight, top_salient_texts_trigram_block, weight_trigram_block
-    
+
     return top_salient_texts, top_weight
 
 
-    
+
 def flatten(t):
-    return [item for sublist in t for item in sublist]  
+    return [item for sublist in t for item in sublist]
 
 
 def dedupe_doc_text(seq):
@@ -165,7 +165,7 @@ def dedupe_doc_text(seq):
     seen_add = seen.add
     return [x.replace('\n','') for x in seq if not (x in seen or seen_add(x))]
 
- 
+
 def calculate_similarity_bert_score(
         model_bertscore,
         tokenizer_bertscore,
@@ -183,22 +183,22 @@ def calculate_similarity_bert_score(
     # create an array of sentences of length no_salient_texts*num_sentences
     sentences_compare_array = flatten(np.array([doc_text for gen_text in salient_texts]).astype('str'))
     P_sci, R_sci, F1_sci = bert_score.score(
-        salient_texts_compare_array, 
+        salient_texts_compare_array,
         sentences_compare_array,
         model_bertscore,
         tokenizer_bertscore,
         model_type,
         num_layers=num_layers,
-        device=device, 
+        device=device,
         verbose=False,
         all_layers=False,
         batch_size=64
     )
     return F1_sci
 
-@timer  
-def calculate_similarity_simsce(model_simcse,salient_texts,doc_text,device):
-    return model_simcse.similarity(list(salient_texts),list(doc_text))
+@timer
+#def calculate_similarity_simsce(model_simcse,salient_texts,doc_text,device):
+    #return model_simcse.similarity(list(salient_texts),list(doc_text))
 
 def calculate_similarity_sentence_transformers(model,salient_texts,doc_text):
     vectors_bert_sent = model.encode(doc_text)
@@ -208,7 +208,7 @@ def calculate_similarity_sentence_transformers(model,salient_texts,doc_text):
         for idx_2, jj in enumerate(vectors_bert_gen_text):
             scores_marix[idx_2,idx_1] =  spatial.distance.cosine(ii, jj)
     return scores_marix
-    
+
 
 def rank_answers_based_on_similarity_scores(doc_text,salient_texts,scores,similarity_model_name,gen_text_weights=np.array([])):
     """
@@ -216,13 +216,13 @@ def rank_answers_based_on_similarity_scores(doc_text,salient_texts,scores,simila
         doc_text: Array[<string>] : array of sentences in article
         salient_texts: Array[<string>] : array of salient_texts summarising the article
         scores: Array[<number>] : np.array of simialirty scores between each sentence and each gen_text
-        gen_text_weights Array[<number>] : np.array of weights associated with importance of each gen_text 
-        
+        gen_text_weights Array[<number>] : np.array of weights associated with importance of each gen_text
+
     Returns:
-        sorted_idxs: Array[<int>] : np.array of indicies of sentencs in doc_text, 
-                     sorted in order of importance for summary 
+        sorted_idxs: Array[<int>] : np.array of indicies of sentencs in doc_text,
+                     sorted in order of importance for summary
     """
-    # reshape so that rows represent different salient_texts and columns represent different sentences in source text 
+    # reshape so that rows represent different salient_texts and columns represent different sentences in source text
     scores_reshaped = np.array(scores.reshape(len(salient_texts),len(doc_text)))
     # optionally multiply by weights associated with salient_texts
     if len(gen_text_weights>0):
@@ -231,7 +231,7 @@ def rank_answers_based_on_similarity_scores(doc_text,salient_texts,scores,simila
     scores_average = np.mean(scores_reshaped,axis=0)
     # sort indicies of scores in descendng order
     if similarity_model_name =='sentence_transformers':
-        # distance based - minimize scores      
+        # distance based - minimize scores
         sorted_idxs = np.argsort(scores_average)
     else:
         # siimilarity based - maximize scores
@@ -250,18 +250,18 @@ def select_sentences(
     """
     Params:
         doc_text: Array[<string>] : array of sentences in article
-        sorted_idxs: Array[<int>] : np.array of indicies of sentencs in doc_text, 
-                     sorted in order of importance for summary 
+        sorted_idxs: Array[<int>] : np.array of indicies of sentencs in doc_text,
+                     sorted in order of importance for summary
         metric: 'tokens' or 'sentences' : how to calculate how long summary should be
         target_tokens: int : number of tokens to aim for in target summary
         target_tokens: int : number of sentences to aim for in target summary
-        gen_text_weights: Array[<number>] : np.array of weights associated with importance of each gen_text 
-        block_n_gram: int or None: if int, number of consecutive words 
+        gen_text_weights: Array[<number>] : np.array of weights associated with importance of each gen_text
+        block_n_gram: int or None: if int, number of consecutive words
                       required to match for a sentence to be blocked
-        
+
     Returns:
-        sorted_idxs: Array[<int>] : np.array of indicies of sentencs in doc_text, 
-                     sorted in order of importance for summary 
+        sorted_idxs: Array[<int>] : np.array of indicies of sentencs in doc_text,
+                     sorted in order of importance for summary
     """
     if metric == 'tokens':
         len_sentences = np.array([len(nltk.word_tokenize(s)) for s in doc_text])
@@ -270,8 +270,8 @@ def select_sentences(
     elif metric == 'sentences':
         article_len = len(doc_text)
         max_len = top_k_sentences
-        
-    #  if article is too short, whole things is summary 
+
+    #  if article is too short, whole things is summary
     if article_len <= max_len:
         pred = doc_text
     else :
@@ -296,14 +296,14 @@ def select_sentences(
         doc_text = np.array(doc_text)
         pred  = doc_text[sorted_pred_idxs]
     return pred
-  
+
 def pick_top_sentences_and_join_into_one_str(
     model,
     tokenizer,
     doc_text,
     salient_texts,
     device,
-    num_layers, 
+    num_layers,
     metric,
     top_k_sentences,
     weights,
@@ -315,10 +315,10 @@ def pick_top_sentences_and_join_into_one_str(
     """
     Passed an array of strings representing an article  (doc_text)
     and an array of strings representing the salient_texts (salient_texts) which summarise it,
-    the function returns one string containing the top k scoring sentences to be included in a summary, 
+    the function returns one string containing the top k scoring sentences to be included in a summary,
     in the order that they appear in the text.
     """
-            
+
     # calculate similarity
     with io.capture_output() as captured:
         if similarity_model_name == 'bert_score':
@@ -331,19 +331,19 @@ def pick_top_sentences_and_join_into_one_str(
                 num_layers,
                 device
             )
-        elif similarity_model_name == 'simcse':
-            scores = calculate_similarity_simsce(
-                model,
-                salient_texts,
-                doc_text,
-                device
-            )
+        #elif similarity_model_name == 'simcse':
+            #scores = calculate_similarity_simsce(
+                #model,
+                #salient_texts,
+                #doc_text,
+                #device
+            #)
         elif similarity_model_name =='sentence_transformers':
             scores = calculate_similarity_sentence_transformers(model,salient_texts,doc_text)
         else:
-            raise ValueError('similarity_model name not recognised.')    
-    
-    #  sort indexes of doc_text sentences in order of importance   
+            raise ValueError('similarity_model name not recognised.')
+
+    #  sort indexes of doc_text sentences in order of importance
     sorted_idxs = rank_answers_based_on_similarity_scores(
         doc_text,
         salient_texts,
@@ -351,7 +351,7 @@ def pick_top_sentences_and_join_into_one_str(
         similarity_model_name,
         gen_text_weights=weights
     )
-    
+
     # select sentences based on requires number of sentences or
     # tokens and integrate optional trigram blocking
     pred = select_sentences(
@@ -363,7 +363,7 @@ def pick_top_sentences_and_join_into_one_str(
         block_n_gram=block_n_gram
         )
     pred_ext_summary = combine_array_sentences(pred)
-    
+
     return pred_ext_summary, sorted_idxs
 
 
@@ -407,8 +407,8 @@ def test_rouge(predicted_summaries, gold_summaries):
     finally:
         pass
         if os.path.isdir(tmp_dir):
-            shutil.rmtree(tmp_dir) 
-    
+            shutil.rmtree(tmp_dir)
+
 def format_rouge_results(results):
     return f"ROUGE-F(1/2/l)/ROUGE-R(1/2/l): {results['rouge_1_f_score']}/{results['rouge_2_f_score']}/{results['rouge_l_f_score']} /{results['rouge_1_recall']}/{results['rouge_2_recall']}/{results['rouge_l_recall']}"
 
@@ -432,7 +432,7 @@ def _block_n_gram(n, c, p,salient_texts=False):
         n: int : number of consecutive words required to match for a sentence to be blocked
         c: string : candidate string to compare to array of strings
         p: Array[<string>]: array of strings to compare to
-        q: Bool: indicates whether the canddiate and prediction strings are salient_texts 
+        q: Bool: indicates whether the canddiate and prediction strings are salient_texts
     """
     tri_c = _get_ngrams(n, c.split())
     for idx, s in enumerate(p):
@@ -448,12 +448,12 @@ def _block_n_gram(n, c, p,salient_texts=False):
 
 
 def get_T5_model(model,device):
-    
+
     tokenizer_T5 = T5Tokenizer.from_pretrained(model)
     model_T5 = T5ForConditionalGeneration.from_pretrained(model)
     output = model_T5.to(device)
     return model_T5, tokenizer_T5
-    
+
 
 @timer
 def main(df,
@@ -482,13 +482,13 @@ def main(df,
     gold_summaries = []
     our_predictions = []
     our_summary_lens = []
-    
+
     for idx, row in tqdm(df.iterrows(),total=len(df)):
-        #  read in article to summarise       
+        #  read in article to summarise
         doc_text = np.array(ast.literal_eval(row[col_name]))
 
         weights = None
-        #  generate salient text fragments  
+        #  generate salient text fragments
         if (block_n_gram_generated_texts):
             salient_texts, freq, q_tg, f_tg = get_salient_texts_across_corpus(
                 generative_model,
@@ -517,14 +517,14 @@ def main(df,
             )
             weights = freq if (gen_text_weights) else np.array([])
 
-        #  generate summary       
+        #  generate summary
         pred_sum, idxs = pick_top_sentences_and_join_into_one_str(
             similarity_model,
             similarity_tokenizer,
             doc_text,
             salient_texts,
             device,
-            num_layers=num_layers,  
+            num_layers=num_layers,
             metric=summary_len_metric,
             top_k_sentences=num_sentences,
             weights=weights,
@@ -534,21 +534,21 @@ def main(df,
             similarity_model_path=similarity_model_path
         )
 
-        #  append out predicted summary and gold summary to arrays for evaluation       
+        #  append out predicted summary and gold summary to arrays for evaluation
         our_summary_lens.append(len(nltk.word_tokenize(pred_sum)))
-        our_predictions.append(pred_sum)       
+        our_predictions.append(pred_sum)
         gold_sum = df.loc[idx,'summary_text_combined']
         gold_summaries.append(gold_sum)
-        
+
 
     #  calculate ROUGE scores
     if (save_predictions):
         with open('./results.json','w') as f:
             json.dump(our_predictions,f)
-    if not (inference_only):   
+    if not (inference_only):
         model_type = similarity_model_path.split('/')[-1]
         our_pred = test_rouge(our_predictions,gold_summaries)
-        
+
         # print summaries
         our_summary_lens = np.array(our_summary_lens)
         print(f'\n\nData col: {col_name}.\n'+
@@ -569,9 +569,9 @@ def main(df,
 
 
 if __name__=='__main__':
-    
-    # -------- DEFINE EXPERIMENT PARAMS --------------- 
-    
+
+    # -------- DEFINE EXPERIMENT PARAMS ---------------
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--num_generated_texts",default=10)
     parser.add_argument("--block_n_gram_generated_texts",default=None)
@@ -592,24 +592,24 @@ if __name__=='__main__':
     parser.add_argument('--inference_only',default=False)
     parser.add_argument("--save_predictions",default=False)
     args = parser.parse_args()
-    
-    # data params  
+
+    # data params
     path = args.data_path
     col_name = args.col_name
-    
-    # gen_text generation params     
+
+    # gen_text generation params
     num_generated_texts = int(args.num_generated_texts)
     block_n_gram_generated_texts = int(args.block_n_gram_generated_texts) if (args.block_n_gram_generated_texts != None) else None
     temperature = float(args.temperature)
     num_texts_per_section = int(args.texts_per_section)
     stride = int(args.stride)
     generative_model_base = args.generative_model_path
-    
+
     # extractive summarisation (similarity and ranking) params
     num_sentences = int(args.num_sentences)
     summary_len_metric = args.summary_len_metric
     similarity_model_name = args.similarity_model_name
-    similarity_model_path = args.similarity_model_path                                                
+    similarity_model_path = args.similarity_model_path
     block_n_gram_sum = int(args.block_n_gram_sum) if (args.block_n_gram_sum != None) else None
     target_tokens = int(args.target_tokens)
     gen_text_weights = bool(args.gen_text_weights)
@@ -620,12 +620,12 @@ if __name__=='__main__':
 
 
 
-    # -------- LOAD MODELS ---------------  
-    
+    # -------- LOAD MODELS ---------------
+
     # Define the target device. Use GPU if available.
     os.environ["CUDA_VISIBLE_DEVICES"] = args.visible_device
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    
+
 
     # define model for gen_text generation
     generative_model, generative_tokenizer = get_T5_model(generative_model_base,device)
@@ -637,21 +637,21 @@ if __name__=='__main__':
             device,
             all_layers=False
         )
-    if (similarity_model_name =='simcse'):
-        similarity_model = SimCSE(similarity_model_path)
-        similarity_tokenizer, num_layers = None, None 
+    #if (similarity_model_name =='simcse'):
+        #similarity_model = SimCSE(similarity_model_path)
+        #similarity_tokenizer, num_layers = None, None
     if (similarity_model_name=='sentence_transformers'):
         similarity_model = SentenceTransformer(similarity_model_path)
         similarity_model.to(device)
-        similarity_tokenizer, num_layers = None, None 
-    
-    
-    # -------- LOAD DATA ---------------  
-      
-    df = pd.read_csv(path)
-    
+        similarity_tokenizer, num_layers = None, None
 
-    # -------- RUN EXPERIMENT --------------- 
+
+    # -------- LOAD DATA ---------------
+
+    df = pd.read_csv(path)
+
+
+    # -------- RUN EXPERIMENT ---------------
 
     main(df,
         generative_model,
